626529bbfcc2076184e7a74e147f746ebc01e5c1
Lorenzo Pini <lore.pini@tin.it>
2014-10-30 18:58:30 +0100

    Refresh paired list and improved click listener

--- src/it/geosolutions/android/loneworker/DeviceListActivity.java
+++ src/it/geosolutions/android/loneworker/DeviceListActivity.java
@@ -90,7 +90,7 @@ public class DeviceListActivity extends Activity {
 
         // Get the local Bluetooth adapter
         mBtAdapter = BluetoothAdapter.getDefaultAdapter();
-
+/*
         // Get a set of currently paired devices
         Set<BluetoothDevice> pairedDevices = mBtAdapter.getBondedDevices();
 
@@ -104,6 +104,8 @@ public class DeviceListActivity extends Activity {
             String noDevices = getResources().getText(R.string.none_paired).toString();
             mPairedDevicesArrayAdapter.add(noDevices);
         }
+        */
+        refreshPairedList();
     }
 
     @Override
@@ -125,20 +127,24 @@ public class DeviceListActivity extends Activity {
     private void doDiscovery() {
         if (D) Log.d(TAG, "doDiscovery()");
 
-        // Indicate scanning in the title
-        setProgressBarIndeterminateVisibility(true);
-        setTitle(R.string.scanning);
-
-        // Turn on sub-title for new devices
-        findViewById(R.id.title_new_devices).setVisibility(View.VISIBLE);
-
-        // If we're already discovering, stop it
-        if (mBtAdapter.isDiscovering()) {
-            mBtAdapter.cancelDiscovery();
+        if(mBtAdapter != null){
+	        // Indicate scanning in the title
+	        setProgressBarIndeterminateVisibility(true);
+	        setTitle(R.string.scanning);
+	
+	        // Turn on sub-title for new devices
+	        findViewById(R.id.title_new_devices).setVisibility(View.VISIBLE);
+	
+	        // If we're already discovering, stop it
+	        if (mBtAdapter.isDiscovering()) {
+	            mBtAdapter.cancelDiscovery();
+	        }
+	
+	        // Request discover from BluetoothAdapter
+	        mBtAdapter.startDiscovery();
+        }else{
+        	// TODO: the BlueTooth adapter has not been initialized, inform the user
         }
-
-        // Request discover from BluetoothAdapter
-        mBtAdapter.startDiscovery();
     }
 
     // The on-click listener for all devices in the ListViews
@@ -151,13 +157,16 @@ public class DeviceListActivity extends Activity {
             String info = ((TextView) v).getText().toString();
             String address = info.substring(info.length() - 17);
 
-            // Create the result Intent and include the MAC address
-            Intent intent = new Intent();
-            intent.putExtra(EXTRA_DEVICE_ADDRESS, address);
-
-            // Set result and finish this Activity
-            setResult(Activity.RESULT_OK, intent);
-            finish();
+            // Check if it is a valid BlueTooth Address string
+            if(BluetoothAdapter.checkBluetoothAddress(address)){
+	            // Create the result Intent and include the MAC address
+	            Intent intent = new Intent();
+	            intent.putExtra(EXTRA_DEVICE_ADDRESS, address);
+	
+	            // Set result and finish this Activity
+	            setResult(Activity.RESULT_OK, intent);
+	            finish();
+            }
         }
     };
 
@@ -184,8 +193,34 @@ public class DeviceListActivity extends Activity {
                     String noDevices = getResources().getText(R.string.none_found).toString();
                     mNewDevicesArrayAdapter.add(noDevices);
                 }
+                
+                // Refresh the main list
+                refreshPairedList();
+                
             }
         }
     };
+    
+    
+    public void refreshPairedList(){
+    	if(mBtAdapter != null){
+	    	// Get a set of currently paired devices
+	        Set<BluetoothDevice> pairedDevices = mBtAdapter.getBondedDevices();
+	
+	        // Clear current list before populating it
+	        mPairedDevicesArrayAdapter.clear();
+	        
+	        // If there are paired devices, add each one to the ArrayAdapter
+	        if (pairedDevices.size() > 0) {
+	            findViewById(R.id.title_paired_devices).setVisibility(View.VISIBLE);
+	            for (BluetoothDevice device : pairedDevices) {
+	                mPairedDevicesArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+	            }
+	        } else {
+	            String noDevices = getResources().getText(R.string.none_paired).toString();
+	            mPairedDevicesArrayAdapter.add(noDevices);
+	        }
+    	}
+    }
 
 }
